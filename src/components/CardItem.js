b18fbdedae33a8990ffab988f44dd98148f61b40
Veton Milaimi <vetonmila@gmail.com>
2021-01-13 11:37:30 +0100

    Configured something in cardItem.js

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function CardItem(props) {
     return (
         <>
-            <li className="cards__items">
+            <li className="cards__item">
                 <Link className="cards__item__link" to={props.path}>
                     <figure className="cards__item__pic-wrap" data-category={props.label}>
                         <img src={props.src} alt="Duraj-com" className="cards__item__img" />
